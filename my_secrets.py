4bb93a6d3d4267c814e6bcdc2f55957b0c9a7de3
CaymenH <158457020+CaymenH@users.noreply.github.com>
2024-05-07 22:02:57 +0100

    Add files via upload

---
+++ my_secrets.py
@@ -0,0 +1,13 @@
+#my_secrets.py
+ssid = "SKYZWEEG"
+password = "515q6Q17KSMp"
+#university ssid and password
+#ssid = "netlab-2.4"
+#password = "0102030405"
+
+
+
+hivemq_host = "0a48cdf916404edea8461de766143f32.s1.eu.hivemq.cloud"
+hivemq_name = "caymen"
+hivemq_pw = "Rubythedog1%"
+hivemq_id = "caymenshive"
